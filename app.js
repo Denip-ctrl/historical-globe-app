cc44d9229afc33505e5b7f1eccd4fd5b94b86c43
Denip-ctrl <denip2016@gmail.com>
2026-09-05 23:10:35 +0700

    Update globe image URL to use blue marble

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ const historicalData = {
 const container = document.getElementById('globeViz');
 const globe = Globe()
   (container)
-  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-water.png')
+  .globeImageUrl('https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-blue-marble.jpg')
   .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
   .htmlElementsData([]) // Untuk nanti menampilkan label/marker di atas globe
   .htmlLat(d => d.lat)

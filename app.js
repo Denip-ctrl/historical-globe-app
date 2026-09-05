c826b6303420f96b473d295a34c5224f7ab60b5a
Denip-ctrl <denip2016@gmail.com>
2026-09-05 23:26:25 +0700

    Update globe image URL in app.js

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ const historicalData = {
 const container = document.getElementById('globeViz');
 const globe = Globe()
   (container)
-  .globeImageUrl('https://unpkg.com/three-globe@2.27.3/example/img/earth-blue-marble.jpg')
+  .globeImageUrl('https://raw.githubusercontent.com/turban/webgl-earth/master/images/2$/*')
   .bumpImageUrl(null)
   .htmlElementsData([]) // Untuk nanti menampilkan label/marker di atas globe
   .htmlLat(d => d.lat)
